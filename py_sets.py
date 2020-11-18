41eaffac59893b63e0bbe934c30039d272aa5b6d
Edward Tanguay (wk) <edward@tanguay.info>
2020-11-18 15:07:11 +0100

    rename cmd to py

---
+++ py_sets.py
@@ -0,0 +1,9 @@
+persons = {"Plato", "Cicero", "Aristotles"}
+print(persons)
+
+# always a different order
+for person in persons:
+	print(person)
+
+if ("Cicero" in persons):
+	print("Yes, Cicero is in the set")
---
+++ py_tuples.py
@@ -0,0 +1,8 @@
+fruits = ("apple", "banana", "orange")
+cities = ("Berlin",) # comma necessary
+things = ("test", 34, True)
+print(fruits)
+print(cities)
+print(things)
+print(len(fruits))
+print(type(cities)) # a tuple is a class 
