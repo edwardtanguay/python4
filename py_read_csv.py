d23df06e76690092dbd036efe273182c26bb80a0
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-11-18 23:45:20 +0100

    some changes

--- py_read_csv.py
+++ py_read_csv.py
@@ -1,11 +1,14 @@
 data = []
-file = open("data/text.csv", "r")
-for line in file:
-	data.append(line.split(";"))
-file.close()
-
-print(data)
-print("-" * 50)
-print(data[2:3])
-print("-" * 50)
-print(data[2][1])
+try:
+	file = open("data/text2.csv", "r")
+	for line in file:
+		data.append(line.split(";"))
+	file.close()
+except FileNotFoundError as e:
+	print("This file was not found: ", e)
+else:
+	print(data)
+	print("-" * 50)
+	print(data[2:3])
+	print("-" * 50)
+	print(data[2][1])
